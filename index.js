378fd377e9618b98b1eab2d34a84a0dabd5f4215
john-paul.vistal <johnpaulvistal@gmail.com>
2020-11-27 21:12:02 +0800

    Initial Commit

--- index.js
+++ index.js
@@ -1,18 +1,123 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+app.set('view engine', 'ejs')
+
+let userName = ""
+let userImg = ""
+let activeUser = []
+let connectCounter = 0
+
+var publicDir = require('path').join(__dirname, '/public');
+app.use(express.static(publicDir));
+
+app.get('/strat.com', function(req, res) {
+    res.render("login", { error: req.query.error })
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
+app.post('/strat-chat-body', function(req, res) {
+    userName = req.body.userName
+    userImg = req.body.userImg
+    res.render("main", {
+        userName: userName,
+        userImg: userImg
+    })
 });
 
-http.listen(port, function(){
-  console.log('listening on *:' + port);
+function formatAMPM(date) {
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    var strTime = hours + ':' + minutes + ' ' + ampm;
+    return strTime;
+}
+
+
+
+
+function addOnline() {
+    return connectCounter += 1
+}
+
+function reduceOnline() {
+    return connectCounter -= 1
+}
+
+function getActiveUsers() {
+    let result = activeUser.findIndex(user => user.userName == userName)
+    if (result == -1 && userName != "") {
+        activeUser.push({ userName: userName, userImg: userImg })
+    }
+    return activeUser
+}
+
+function leaveUser(name) {
+    let result = activeUser.findIndex(user => user.userName == name)
+    activeUser.splice(result, 1)
+    return activeUser
+}
+
+
+io.on('connection', function(socket) {
+    io.emit('active', getActiveUsers())
+
+    io.emit("count", addOnline())
+
+    socket.on('message', function(msg) {
+        io.emit('message', msg);
+    });
+
+    socket.on('typing', function(typing) {
+        socket.broadcast.emit('typing', typing)
+    });
+
+    socket.on('untyping', function() {
+        socket.broadcast.emit('untyping')
+    });
+
+
+    //Broadcast if a new user connect
+    let date = new Date()
+    let dateStamp = date.getFullYear() + "-" + date.getMonth() + 1 + "-" + date.getDate();
+    let timeStamp = formatAMPM(date)
+
+    socket.broadcast.emit("message", {
+        "sender": "StratBOT",
+        "message": `${userName}  joined the chat!`,
+        "date": dateStamp,
+        "time": timeStamp,
+    })
+
+    socket.on('leave', function(name) {
+        io.emit('active', leaveUser(name))
+        io.emit("count", reduceOnline())
+    })
+
+
+    //Broadcast if a user disconnect
+    socket.on('disconnect', function() {
+        io.emit("message", {
+            "sender": "StratBOT",
+            "message": `${userName}  left the chat!`,
+            "date": dateStamp,
+            "time": timeStamp,
+        })
+        io.emit('leave', userName);
+    });
+
+});
+
+
+http.listen(port, function() {
+    console.log('listening on *:' + port);
 });
